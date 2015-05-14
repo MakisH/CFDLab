7ce7090c488b82a05e41c21d63ad44788e76341e
IgorTo <igor.tominec@gmail.com>
2015-05-14 15:30:47 +0200

    implementation of doCollisions function

--- Worksheet 02/collision.c
+++ Worksheet 02/collision.c
@@ -1,10 +1,35 @@
 #include "collision.h"
+#include "LBDefinitions.h"
+
 
 void computePostCollisionDistributions(double *currentCell, const double * const tau, const double *const feq){
   /* TODO */
 }
 
 void doCollision(double *collideField, int *flagField,const double * const tau,int xlength){
-  /* TODO */
+
+	int x, y, z;
+
+	double density;
+	double velocity[3];
+	double feq[Q_NUMBER];
+	double *currentCell;
+
+	for (x = 1; x < xlength + 1; x++){
+		for (y = 1; y < xlength + 1; y++){
+			for (z = 1; z < xlength + 1; z++){
+
+				// address to the -> first <- distribution function within the respective cell.
+				currentCell = collideField + Q_NUMBER * (x + xlength*y + xlength*xlength*z);
+
+				computeDensity (currentCell, &density);
+				computeVelocity (currentCell, &density, velocity);
+				computeFeq (&density, velocity, feq);
+				computePostCollisionDistributions (currentCell, tau, feq);
+			}
+		}
+	}
+
+
 }
 

fd3f812fb69cda41d0a58ed9098951eebf280a46
IgorTo <igor.tominec@gmail.com>
2015-05-15 21:38:23 +0200

    bla

--- Worksheet 02/LBDefinitions.h
+++ Worksheet 02/LBDefinitions.h
@@ -1,9 +1,10 @@
 #ifndef _LBDEFINITIONS_H_
 #define _LBDEFINITIONS_H_
+#include <math.h>
 
   static const int LATTICEVELOCITIES[19][3];
   static const double LATTICEWEIGHTS[19];
-  static const double C_S;
+  static const double C_S = 1.0 / sqrt(3.0);
 
   static const unsigned int Q_NUMBER = 19;
   static const unsigned int FLUID = 0;

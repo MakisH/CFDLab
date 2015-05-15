7f18c116d87c6947faf1ec9b2c7dff8e62b84705
martosss <martosss@abv.bg>
2015-05-15 03:33:57 +0200

    Update computeCellValues.c

--- Worksheet 02/computeCellValues.c
+++ Worksheet 02/computeCellValues.c
@@ -2,21 +2,15 @@
 #include "LBDefinitions.h"
 
 void computeDensity(const double *const currentCell, double *density){
-/** computes the density from the pdf-s stored at currentCell.
- *  currentCell thus denotes the address of the first pdf of the
- *  respective cell. The result is stored in density.
- */
 	*density = *currentCell;
 	for(unsigned int i = 1; i < Q_NUMBER; ++i)
 		*density += currentCell[i];
 }
 
 void computeVelocity(const double * const currentCell, const double * const density, double *velocity){
-  /** computes the velocity within currentCell and stores the result in velocity */
-	
 // if this is true, it can be heavily optimized!
 // Table1(page3) - loop i => +/-/0 depend on currentCell ( use enums somehow ?)
-	velocity[0]	= *currentCell * LATTICEVELOCITIES[0][0];
+	velocity[0] = *currentCell * LATTICEVELOCITIES[0][0];
 	velocity[1] = *currentCell * LATTICEVELOCITIES[0][1];
 	velocity[2] = *currentCell * LATTICEVELOCITIES[0][2];
 	for(unsigned int i = 1; i < Q_NUMBER; ++i){

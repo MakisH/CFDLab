299415f30f869388f265c87baf5a10360ea12611
IgorTo <igor.tominec@gmail.com>
2015-05-16 01:17:32 +0200

    static const on sqrt(3) removed

--- Worksheet 02/LBDefinitions.h
+++ Worksheet 02/LBDefinitions.h
@@ -6,7 +6,6 @@
 
   static const int LATTICEVELOCITIES[Q_NUMBER][3];
   static const double LATTICEWEIGHTS[Q_NUMBER];
-  static const double C_S = 1.0/sqrt(3.0);
 
   static const int LATTICEVELOCITIES[19][3] = {{0, -1, -1}, {-1, 0, -1}, {0, 0, -1}, {1, 0, -1}, {0, 1, -1}, {-1, -1, 0}, {0, -1, 0}, {1, -1, 0}, {-1, 0, 0}, {0, 0, 0}, {1, 0, 0}, {-1, 1, 0}, {0, 1, 0}, {1, 1, 0}, {0, -1, 1}, {-1, 0, 1}, {0, 0, 1}, {1, 0, 1}, {0, 1, 1}};
   static const double LATTICEWEIGHTS[19] = {1.0/36, 1.0/36, 2.0/36, 1.0/36, 1.0/36, 1.0/36, 2.0/36, 1.0/36, 2.0/36, 12.0/36, 2.0/36, 1.0/36, 2.0/36, 1.0/36, 1.0/36, 1.0/36, 2.0/36, 1.0/36, 1.0/36};

b7d416e996368edef8180ca54bdf01b90fc8cd70
martossss <martosss>
2015-05-15 03:47:03 +0200

    fix ; and * - our favorite!

--- Worksheet 02/computeCellValues.c
+++ Worksheet 02/computeCellValues.c
@@ -30,12 +30,12 @@ void computeFeq(const double * const density, const double * const velocity, dou
 
   for (int i=0; i<Q_NUMBER; i++) {
     // precalculate inner product c*u
-    double c_u = LATTICEVELOCITIES[i][1] * velocity[1] +   LATTICEVELOCITIES[i][2] * velocity[2] +   LATTICEVELOCITIES[i][3] * velocity[3]
+    double c_u = LATTICEVELOCITIES[i][1] * velocity[1] +   LATTICEVELOCITIES[i][2] * velocity[2] +   LATTICEVELOCITIES[i][3] * velocity[3];
     // calculate the equilibrium distribution element
-    feq[i] = LATTICEWEIGHTS[i] * density * (  1 
+    feq[i] = LATTICEWEIGHTS[i] * *density * (  1 
                                               + c_u / C_S_sq
                                               + c_u * c_u / ( 2 * C_S_sq * C_S_sq )
-                                              - u_u / ( 2 * C_S_sq )   )           
+                                              - u_u / ( 2 * C_S_sq )   );           
   }
 }
 

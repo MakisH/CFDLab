9bedf59dd96d7f3d05034070016b300abadf4df2
martossss <martosss>
2015-05-17 00:07:54 +0200

    LBDefinitions double declaration...there was a mistake after all.

--- Worksheet 02/LBDefinitions.h
+++ Worksheet 02/LBDefinitions.h
@@ -4,19 +4,16 @@
 
 #define Q_NUMBER 19
 
-  static const int LATTICEVELOCITIES[Q_NUMBER][3];
-  static const double LATTICEWEIGHTS[Q_NUMBER];
+	static const int LATTICEVELOCITIES[Q_NUMBER][3] = {{0, -1, -1}, {-1, 0, -1}, {0, 0, -1}, {1, 0, -1}, {0, 1, -1}, {-1, -1, 0}, {0, -1, 0}, {1, -1, 0}, {-1, 0, 0}, {0, 0, 0}, {1, 0, 0}, {-1, 1, 0}, {0, 1, 0}, {1, 1, 0}, {0, -1, 1}, {-1, 0, 1}, {0, 0, 1}, {1, 0, 1}, {0, 1, 1}};
+	static const double LATTICEWEIGHTS[Q_NUMBER] = {1.0/36, 1.0/36, 2.0/36, 1.0/36, 1.0/36, 1.0/36, 2.0/36, 1.0/36, 2.0/36, 12.0/36, 2.0/36, 1.0/36, 2.0/36, 1.0/36, 1.0/36, 1.0/36, 2.0/36, 1.0/36, 1.0/36};
 
-  static const int LATTICEVELOCITIES[19][3] = {{0, -1, -1}, {-1, 0, -1}, {0, 0, -1}, {1, 0, -1}, {0, 1, -1}, {-1, -1, 0}, {0, -1, 0}, {1, -1, 0}, {-1, 0, 0}, {0, 0, 0}, {1, 0, 0}, {-1, 1, 0}, {0, 1, 0}, {1, 1, 0}, {0, -1, 1}, {-1, 0, 1}, {0, 0, 1}, {1, 0, 1}, {0, 1, 1}};
-  static const double LATTICEWEIGHTS[19] = {1.0/36, 1.0/36, 2.0/36, 1.0/36, 1.0/36, 1.0/36, 2.0/36, 1.0/36, 2.0/36, 12.0/36, 2.0/36, 1.0/36, 2.0/36, 1.0/36, 1.0/36, 1.0/36, 2.0/36, 1.0/36, 1.0/36};
+	static const double C_S = 0.577350269189626;
 
-  static const double C_S = 0.577350269189626;
+	static const unsigned int FLUID = 0;
+	static const unsigned int NO_SLIP = 1;
+	static const unsigned int MOVING_WALL = 2;
 
-  static const unsigned int FLUID = 0;
-  static const unsigned int NO_SLIP = 1;
-  static const unsigned int MOVING_WALL = 2;
-
-  // Precalculations for performance
-  static const double C_S_sq = 0.577350269189626 * 0.577350269189626;
+	// Precalculations for performance
+	static const double C_S_sq = 0.577350269189626 * 0.577350269189626;
 
 #endif

edf14b5ab80d0dbfbd71276754bcbeb680087fe0
IgorTo <igor.tominec@gmail.com>
2015-05-17 00:29:23 +0200

    for loop range fix

--- Worksheet 02/streaming.c
+++ Worksheet 02/streaming.c
@@ -2,6 +2,7 @@
 #include "LBDefinitions.h"
 
 void doStreaming(double *collideField, double *streamField,int *flagField,int xlength){
+
 	unsigned int xlength2 = (xlength + 2) * (xlength + 2);
 	for(unsigned int i = 1; i <= xlength; ++i){								// iterate through all FLUID cells
 		unsigned int ixx = xlength2 * i;									// improve code readability and optimize computations
@@ -9,7 +10,7 @@ void doStreaming(double *collideField, double *streamField,int *flagField,int xl
 			unsigned int jx = (xlength + 2) * j;							// improve code readability and optimize computations
 			for(unsigned int k = 1; k <= xlength; ++k){
 				unsigned int current_idx = (k + jx + ixx) * Q_NUMBER;
-				for(unsigned int Q_iter = 0; Q_iter <= Q_NUMBER; ++Q_iter){	// iterate through all directions
+				for(unsigned int Q_iter = 0; Q_iter < Q_NUMBER; ++Q_iter){	// iterate through all directions
 					// - find neighboring cells and take their respective directions ... tricky...
 					// - we need the opposite cell to the Q - direction(hence the "-" sign in front of the offset with Lattice velocities)
 					// with the velocity in the same direction(hence the Q_iter without minus)
@@ -22,4 +23,3 @@ void doStreaming(double *collideField, double *streamField,int *flagField,int xl
 		}
 	}
 }
-

2021e7cac3f7a92b82764953652c0a7db9daeba6
martossss <martosss>
2015-05-16 16:30:32 +0200

    xlength+2 ; index 1,2,3=>0,1,2 ; feq and PostCollisionDistribution reformulation

--- Worksheet 02/collision.c
+++ Worksheet 02/collision.c
@@ -1,13 +1,12 @@
 #include "collision.h"
 #include "LBDefinitions.h"
 
-
 void computePostCollisionDistributions(double *currentCell, const double * const tau, const double *const feq){
   /* TODO */ // Be careful! A C guy should check if what I do really makes sense!
-  for (int i=0; i<Q_NUMBER; i++) {
-        *(currentCell + i) -= ( 1 / *tau ) * ( *(currentCell + i) - feq[i]);
+	// looks good! :)
+  for (int i = 0; i < Q_NUMBER; ++i) {
+		*(currentCell + i) -= ( *(currentCell + i) - feq[i]) / *tau;
   }
-
 }
 
 void doCollision(double *collideField, int *flagField, const double * const tau, int xlength){
@@ -19,12 +18,12 @@ void doCollision(double *collideField, int *flagField, const double * const tau,
 	double feq[Q_NUMBER];
 	double *currentCell;
 
-	for (x = 1; x < xlength + 1; x++){
-		for (y = 1; y < xlength + 1; y++){
-			for (z = 1; z < xlength + 1; z++){
+	for (x = 1; x <= xlength; ++x){
+		for (y = 1; y <= xlength; ++y){
+			for (z = 1; z <= xlength; ++z){
 
 				// address to the -> first <- distribution function within the respective cell.
-				currentCell = collideField + Q_NUMBER * (x + xlength*y + xlength*xlength*z);
+				currentCell = collideField + Q_NUMBER * (z + (xlength + 2) * y + (xlength + 2) * (xlength + 2) * x);
 
 				computeDensity (currentCell, &density);
 				computeVelocity (currentCell, &density, velocity);
--- Worksheet 02/computeCellValues.c
+++ Worksheet 02/computeCellValues.c
@@ -18,24 +18,23 @@ void computeVelocity(const double * const currentCell, const double * const dens
 		velocity[1] += currentCell[i] * LATTICEVELOCITIES[i][1];
 		velocity[2] += currentCell[i] * LATTICEVELOCITIES[i][2];
 	}
-	velocity[0] /=*density;
-	velocity[1] /=*density;
-	velocity[2] /=*density;
+	velocity[0] /= *density;
+	velocity[1] /= *density;
+	velocity[2] /= *density;
 }
 
 void computeFeq(const double * const density, const double * const velocity, double *feq){
-  
-  // precalculate inner product u*u
-  double u_u = velocity[1] * velocity[1] + velocity[2] * velocity[2] + velocity[3] * velocity[3];
+	// velocity indices are 0,1,2(look above)
+	// I hope the feq function is a bit more readable now
 
-  for (int i=0; i<Q_NUMBER; i++) {
-    // precalculate inner product c*u
-    double c_u = LATTICEVELOCITIES[i][1] * velocity[1] +   LATTICEVELOCITIES[i][2] * velocity[2] +   LATTICEVELOCITIES[i][3] * velocity[3];
-    // calculate the equilibrium distribution element
-    feq[i] = LATTICEWEIGHTS[i] * *density * (  1 
-                                              + c_u / C_S_sq
-                                              + c_u * c_u / ( 2 * C_S_sq * C_S_sq )
-                                              - u_u / ( 2 * C_S_sq )   );           
-  }
+	// precalculate inner product u*u
+	double u_u_2c2 = (velocity[0] * velocity[0] + velocity[1] * velocity[1] + velocity[2] * velocity[2]) / (2 * C_S_sq);
+
+	for (int i = 0; i < Q_NUMBER; ++i) {
+		// precalculate inner product c*u
+		double c_u_c2 = (LATTICEVELOCITIES[i][0] * velocity[0] + LATTICEVELOCITIES[i][1] * velocity[1] + LATTICEVELOCITIES[i][2] * velocity[2]) / C_S_sq;
+		// calculate the equilibrium distribution element
+		feq[i] = LATTICEWEIGHTS[i] * *density * (  1 + c_u_c2 * (1 + c_u_c2 / 2) - u_u_2c2);
+	}
 }
 

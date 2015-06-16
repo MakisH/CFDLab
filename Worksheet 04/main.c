b7da3933004e8549395cd102e94fedfae7c01633
IgorTo <igor.tominec@gmail.com>
2015-06-16 13:59:29 +0200

    innerDomain instead of xlength introduced

--- Worksheet 04/main.c
+++ Worksheet 04/main.c
@@ -33,33 +33,39 @@ int main(int argc, char *argv[]){
   // Read the config file
 	readParameters( &xlength[0], &tau, velocityWall, &timesteps, &timestepsPerPlotting, &iProc, &jProc, &kProc, argc, argv ); // reading parameters from the file.
 
+	// Each CPU is going to work in its own subdomain.
+	int innerDomain[3];
+	innerDomain[0] = xlength[0]/iProc + 2;
+	innerDomain[1] = xlength[1]/jProc + 2;
+	innerDomain[2] = xlength[2]/kProc + 2;
+	innerDomain_size = innerDomain[0] * innerDomain[1] * innerDomain[2];
+
 	// Allocating the main three arrays.
-	int domain = (xlength + 2) * (xlength + 2) * (xlength + 2);
-	collideField = (double *) malloc(Q_NUMBER * domain * sizeof(double));
-	streamField = (double *) malloc(Q_NUMBER * domain * sizeof(double));
-	flagField = (int *) malloc(domain * sizeof(int));
+	collideField = (double *) malloc(Q_NUMBER * innerDomain_size * sizeof(double));
+	streamField = (double *) malloc(Q_NUMBER * innerDomain_size * sizeof(double));
+	flagField = (int *) malloc(innerDomain_size * sizeof(int));
 
 	// Init the main three arrays.
-	initialiseFields( collideField, streamField, flagField, xlength );
+	initialiseFields( collideField, streamField, flagField, innerDomain );
 
 	// allocate the buffers
-	initialiseBuffers(sendBuffer, readBuffer, xlength);
+	initialiseBuffers(sendBuffer, readBuffer, innerDomain);
 
 	for(int t = 0; t <= timesteps; t++){
 		double *swap = NULL;
-		doStreaming( collideField, streamField, flagField, xlength );
+		doStreaming( collideField, streamField, flagField, innerDomain );
 
 		swap = collideField;
 		collideField = streamField;
 		streamField = swap;
 
-		doCollision( collideField, flagField, &tau, xlength );
+		doCollision( collideField, flagField, &tau, innerDomain );
 
-		treatBoundary( collideField, flagField, velocityWall, xlength );
+		treatBoundary( collideField, flagField, velocityWall, innerDomain );
 
 		if ( t % timestepsPerPlotting == 0 ) {
       printf("Writing the vtk file for timestep # %d \n", t);
-      writeVtkOutput( collideField, flagField, "pics/simLB", t, xlength );
+      writeVtkOutput( collideField, flagField, "pics/simLB", t, innerDomain );
     }
     
 	}

b9bd52e85bb3e0dc940193a266456116311196b6
IgorTo <igor.tominec@gmail.com>
2015-06-16 14:04:50 +0200

    corrected collision to support multiple dimensions

--- Worksheet 04/collision.c
+++ Worksheet 04/collision.c
@@ -2,36 +2,35 @@
 #include "LBDefinitions.h"
 
 void computePostCollisionDistributions(double *currentCell, const double * const tau, const double *const feq){
-  for (int i = 0; i < Q_NUMBER; ++i) {
+	for (int i = 0; i < Q_NUMBER; ++i) {
 		//currentCell[i] -= ( currentCell[i] - feq[i]) / *tau;
 		currentCell[i] = ( currentCell[i] * (*tau - 1) + feq[i]) / *tau; // 10-15 sec faster ...
-  }
+	}
 }
 
-/* This is a dirty version of doCollision. However, it is implemented like that because it is faster! */
-void doCollision(double *collideField, int *flagField, const double * const tau, int xlength){
-	unsigned int xlength2 = (xlength + 2) * (xlength + 2) * Q_NUMBER;
+void doCollision(double *collideField, int *flagField, const double * const tau, int *xlength){
+
+	int x, y, z;
+
 	double density;
 	double velocity[3];
 	double feq[Q_NUMBER];
+	double *currentCell;
 
-	// move the pointer colllideField instead of calculating its offset every time => 10% faster for 100 grid (15-20sec)
-	double *stop1;
-	double *stop2;
-	double *stop3;
-	unsigned int stop2_offset = Q_NUMBER * (xlength + 2) * xlength;
-	unsigned int stop3_offset = Q_NUMBER * xlength;
-	collideField += xlength2 + (xlength + 3) * Q_NUMBER;
-	for (stop1 			= collideField + xlength2 * xlength;collideField < stop1; collideField += 2 * Q_NUMBER * (xlength + 2)){	// skip y-boundary rows at plane end
-		for (stop2 		= collideField + stop2_offset;		collideField < stop2; collideField += 2 * Q_NUMBER){					// skip x-boundary cells at row end
-			for (stop3 	= collideField + stop3_offset; 		collideField < stop3; collideField += Q_NUMBER){
-				// locally update cells, can be easily parallelized
-				computeDensity (collideField, &density);
-				computeVelocity (collideField, &density, velocity);
-				computeFeq (&density, velocity, feq);
-				computePostCollisionDistributions (collideField, tau, feq);
+	for (z = 1; z <= xlength[2]; ++z){
+		for (y = 1; y <= xlength[1]; ++y){
+			for (x = 1; x <= xlength[0]; ++x){
+
+				if (flagField[x + (xlength[0] + 2) * y + (xlength[0] + 2) * (xlength[1] + 2) * z] == FLUID)
+				{
+					// address to the -> first <- distribution function within the respective cell.
+					currentCell = collideField + Q_NUMBER * (x + (xlength[0] + 2) * y + (xlength[0] + 2) * (xlength[1] + 2) * z);
+					computeDensity (currentCell, &density);
+					computeVelocity (currentCell, &density, velocity);
+					computeFeq (&density, velocity, feq);
+					computePostCollisionDistributions (currentCell, tau, feq);
+				}
 			}
 		}
 	}
 }
-

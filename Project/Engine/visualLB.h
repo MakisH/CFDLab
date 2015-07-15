6e0a039ac31b0767b74723dede6133bee4128d07
MakisH <makishourdakis@gmail.com>
2015-07-15 13:11:07 +0200

    Adapted visualization functions.

--- Project/Engine/visualLB.h
+++ Project/Engine/visualLB.h
@@ -7,12 +7,10 @@
  *  to a file determined by 'filename' and timestep 't'. You can re-use parts of the code
  *  from visual.c (VTK output for Navier-Stokes solver) and modify it for 3D datasets.
  */
-void writeVtkOutput(const double * const collideField, const int * const flagField, char * filename, unsigned int t, int *xlength, int rank, int *xlength_global,
-										int iProc, int jProc, int kProc);
+void writeVtkOutput(const double * const collideField, const int * const flagField, char *filename, unsigned int t, int *xlength, int part_id);
 
 void write_vtkHeader( FILE *fp, int *xlength);
 
-void write_vtkPointCoordinates( FILE *fp, int *xlength, int *xlength_global, int iProc, int jProc, int kProc, int rank);
+void write_vtkPointCoordinates( FILE *fp, int part_id);
 
 #endif
-
